b58bb1ca6916176e6e3c50926dddc87a841fe38d
sineetg <wrsineety@gmail.com>
2025-06-15 19:32:07 +0300

    Refactor display_balance to return balance string instead of printing

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        return f"Current Balance: ${self.__account_balance}"
